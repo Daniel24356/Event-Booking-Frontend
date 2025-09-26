4cfd10a86b8262fd244377e51725c0c9e635bdcb
Daniel24356 <igwedaniel128@gmail.com>
2025-09-26 13:04:15 -0700

    debugged tailwind issue

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,13 @@
+import Categories from "../Components/Categories"
+import Header from "../Components/Header"
+import Hero from "../Components/Hero"
+
+const Home = () => {
+    return <>
+     <Header/>
+     <Hero/>
+     <Categories/>
+    </>
+}
+
+export default Home
