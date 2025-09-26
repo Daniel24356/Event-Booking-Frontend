2cb1cd2288335d930cf24d422bbd122bacc69f88
Daniel24356 <igwedaniel128@gmail.com>
2025-09-26 13:09:20 -0700

    added swiper

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
             }}
         >
             {/* Overlay */}
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+             <div className="absolute inset-0 bg-black/50"></div>
 
 
             {/* Hero Content */}
